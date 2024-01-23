8a35e1ee9bf8a41769ac3416b6c3ac434e6acd36
takedamasaki555 <masakit@microsoft.com>
2024-01-23 08:31:58 +0000

    test

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['*']
-# os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 if 'CODESPACE_NAME' in os.environ:
     codespace_name = os.getenv("CODESPACE_NAME")
